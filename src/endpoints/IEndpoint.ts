46788c8af6dfa497f500c81f592453e513323758
Christophe Tiraoui <krux@krux.fr>
2022-12-11 16:19:22 +0100

    endpoint design fix

--- src/endpoints/IEndpoint.ts
+++ src/endpoints/IEndpoint.ts
@@ -1,6 +1,6 @@
 import { IKernelElement } from "../kernel/IKernelElement";
 
 export interface IEndpoint extends IKernelElement {
-  renderUI: () => Promise<void>;
-  listenAPI: () => Promise<void>;
+  open: () => Promise<void>;
+  close: () => Promise<void>;
 }
